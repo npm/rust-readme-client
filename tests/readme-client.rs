cd0f8fb0edf8ec8521a87a8da00dd7cfeac7db31
Ashley Williams <ashley666ashley@gmail.com>
2017-11-21 09:31:08 -0500

    feat(scoped): accept scoped pkgs

--- tests/readme-client.rs
+++ tests/readme-client.rs
@@ -1,6 +1,15 @@
 extern crate readme_client;
 
 #[test]
+fn fetch_scoped() {
+    let resp = readme_client::fetch_latest("@ag_dubs/scoped-for-test");
+    assert!(!resp.is_err(), "there should be no error");
+    assert!(resp.unwrap().contains(
+        "<!-- this HTML was generated using marky-markdown",
+    ));
+}
+
+#[test]
 fn fetch_latest_pass() {
     let resp = readme_client::fetch_latest("express");
     assert!(!resp.is_err(), "there should be no error");
