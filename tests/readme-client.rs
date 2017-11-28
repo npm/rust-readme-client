a705a564078805917a9fc989d289a668ea80dc12
Jeffrey Lembeck <jlembeck@gmail.com>
2017-11-28 13:19:30 -0800

    feat(interface): take String instead of slice
    This is due to Rocket's interface
    
    BREAKING CHANGE

--- tests/readme-client.rs
+++ tests/readme-client.rs
@@ -2,7 +2,7 @@ extern crate readme_client;
 
 #[test]
 fn fetch_scoped() {
-    let resp = readme_client::fetch_latest("@ag_dubs/scoped-for-test");
+    let resp = readme_client::fetch_latest(String::from("@ag_dubs/scoped-for-test"));
     assert!(!resp.is_err(), "there should be no error");
     assert!(
         resp.unwrap()
@@ -12,7 +12,7 @@ fn fetch_scoped() {
 
 #[test]
 fn fetch_latest_pass() {
-    let resp = readme_client::fetch_latest("express");
+    let resp = readme_client::fetch_latest(String::from("express"));
     assert!(!resp.is_err(), "there should be no error");
     assert!(
         resp.unwrap()
@@ -22,13 +22,13 @@ fn fetch_latest_pass() {
 
 #[test]
 fn fetch_latest_fail() {
-    let resp = readme_client::fetch_latest("notapkgfartfart");
+    let resp = readme_client::fetch_latest(String::from("notapkgfartfart"));
     assert!(resp.is_err(), "there should be an error");
 }
 
 #[test]
 fn fetch_version_pass() {
-    let resp = readme_client::fetch_version("express", "4.16.2");
+    let resp = readme_client::fetch_version(String::from("express"), String::from("4.16.2"));
     assert!(!resp.is_err(), "there should be no error");
     assert!(
         resp.unwrap()
@@ -38,6 +38,6 @@ fn fetch_version_pass() {
 
 #[test]
 fn fetch_version_fail() {
-    let resp = readme_client::fetch_version("express", "4.10.2");
+    let resp = readme_client::fetch_version(String::from("express"), String::from("4.10.2"));
     assert!(resp.is_err(), "there should be an error");
 }
